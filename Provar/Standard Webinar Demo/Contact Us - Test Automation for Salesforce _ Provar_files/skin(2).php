69cd229372aea5cea252e775fdcb3eb0dab8b369
Richard Clark <richard.clark@provartesting.com>
2018-04-27 16:27:49 +0100

    Add demo signup page linked to Provar Demo Org instead of Live Provar SF Org.

---
+++ Provar/Standard Webinar Demo/Contact Us - Test Automation for Salesforce _ Provar_files/skin(2).php
@@ -0,0 +1,8 @@
+#dc_jqaccordion_widget-4-item{background-color: #fff; border-top: 1px solid #ccc; border-right: 1px solid #ccc; border-left: 1px solid #ccc;}
+#dc_jqaccordion_widget-4-item ul, #dc_jqaccordion_widget-4-item ul, #dc_jqaccordion_widget-4-item ul li {margin: 0; padding: 0; border: none; list-style: none;}
+#dc_jqaccordion_widget-4-item ul a {background-repeat: no-repeat; background-position: 10px center; border-top: 1px solid #fff; border-bottom: 1px solid #ccc; padding: 10px 10px 10px 32px; text-decoration:none; display: block; color: #222; font-weight: bold; color: #4e4e4e;}
+#dc_jqaccordion_widget-4-item ul ul a {font-weight: normal; color: #4e4e4e;}
+#dc_jqaccordion_widget-4-item ul a.dcjq-parent {background-image: url(skins/images/plus_grey.png); font-weight: bold; background-color: #fff; color: #4e4e4e;}
+#dc_jqaccordion_widget-4-item ul a.dcjq-parent.active {background-image: url(skins/images/minus_grey.png); color: #ec008c;}
+#dc_jqaccordion_widget-4-item ul a:hover {color: #ec008c;}
+#dc_jqaccordion_widget-4-item li ul {padding-left: 50px;text-decoration: none;}
